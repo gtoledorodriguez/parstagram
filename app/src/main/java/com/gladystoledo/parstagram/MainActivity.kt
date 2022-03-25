a095ed48c049ac1235b2b921a3ee40804910c10d
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2022-03-25 11:14:03 -0500

    Created Post Post will add a specific user's image and description

--- app/src/main/java/com/gladystoledo/parstagram/MainActivity.kt
+++ app/src/main/java/com/gladystoledo/parstagram/MainActivity.kt
@@ -2,6 +2,10 @@ package com.gladystoledo.parstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 /**
  * Let user create a post by taking a photo with their camera
@@ -10,5 +14,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        queryPosts()
+    }
+
+    //Query for all posts in our server
+    fun queryPosts() {
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+
+        query.include(Post.KEY_USER)
+        query.findInBackground(object: FindCallback<Post>{
+            //Find all Post Objects
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null){
+                    //Something has gone wrong
+                    Log.e(TAG, "Error fetching posts")
+                }else{
+                    if (posts != null){
+                        for (post in posts){
+                            Log.i(TAG, "Post: " + post.getDescription() + ", username: "
+                            + post.getUser()?.username)
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    companion object{
+        const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/com/gladystoledo/parstagram/ParstagramApplication.kt
+++ app/src/main/java/com/gladystoledo/parstagram/ParstagramApplication.kt
@@ -2,11 +2,16 @@ package com.gladystoledo.parstagram
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseObject
 
 class ParstagramApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        ParseObject.registerSubclass(Post::class.java)
+
+
         Parse.initialize(
             Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
